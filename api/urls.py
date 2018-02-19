418208acd0973cd045dfbb7c31390ede43c4a882
郭伟杰 <529844698@qq.com>
2018-02-19 16:05:23 +0800

    增加获取列表类别、页面列表、列表详情接口

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from api.views import get_vrmodes, get_banners
+from api.views import get_vrmodes, get_banners, get_pagetypes, get_pages, get_pagedetail
 
 urlpatterns = [
 
     url(r'^getModes/$', get_vrmodes),
 
     url(r'^getBanners/$', get_banners),
+
+    url(r'^getPageTypes/$', get_pagetypes),
+
+    url(r'^getPages/$', get_pages),
+
+    url(r'^getPageDetail/$', get_pagedetail),
+
+    url(r'^pagedetail/(?P<page_id>\d+)/$', get_pagedetail),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 import simplejson as json
-from vrmode.models import VRMode, VRBanner
+# from datetime import datetime
+from vrmode.models import VRMode, VRBanner, PageType, Page, PAGE_STATUS_ACTIVE
 from common import LOG
 
 def get_vrmodes(request):
@@ -33,5 +34,37 @@ def get_banners(request):
 	data = []
 	for vr in banners.all():
 		data.append({'cover': host+vr.cover.url, 'title': vr.title, 'intro': vr.intro, 'vrlink': host+vr.vrlink})
+	result = {'data': data}
+	return HttpResponse(json.dumps(result), content_type='application/json')
+
+def get_pagetypes(request):
+	types = PageType.objects.filter(status=PAGE_STATUS_ACTIVE).order_by('order')[:6]
+	data = []
+	for pagetype in types.all():
+		data.append({'name': pagetype.name, 'id': pagetype.id, 'order': pagetype.order})
+	result = {'data': data}
+	return HttpResponse(json.dumps(result), content_type='application/json')
+
+def get_pages(request):
+	host = request.get_host()
+	type = request.GET.get('type')
+	page = int(request.GET.get('page', 1))
+	if page < 1:
+		page = 1
+	size = int(request.GET.get('size', 5))
+	if size < 1:
+		size = 1
+	pages = Page.objects.filter(status=PAGE_STATUS_ACTIVE, type__id=type).order_by('order')[(page-1)*size:(page)*size]
+	data = []
+	for page in pages.all():
+		data.append({'title': page.title, 'intro': page.intro, 'order': page.order, 'id': page.id, 'cover': host + page.thumb.url, 'time': page.time_display.strftime( '%Y-%m-%d' )})
+	result = {'data': data}
+	return HttpResponse(json.dumps(result), content_type='application/json')
+
+def get_pagedetail(request, page_id):
+	page = get_object_or_404(Page, id=page_id)
+	host = request.get_host()
+	data = []
+	data.append({'title': page.title, 'intro': page.intro, 'order': page.order, 'id': page.id, 'cover': host + page.thumb.url, 'time': page.time_display.strftime( '%Y-%m-%d' ), 'content': page.content })
 	result = {'data': data}
 	return HttpResponse(json.dumps(result), content_type='application/json')
