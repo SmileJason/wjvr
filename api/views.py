3c53466e6fdd1f2fd8caa0bf65aed3176af17886
郭伟杰 <529844698@qq.com>
2018-02-20 13:44:45 +0800

    初始化项目

--- api/views.py
+++ api/views.py
@@ -75,32 +75,34 @@ def get_pagedetail(request, page_id):
 APP_ID = 'wxe986c48a87b379cd'
 APP_SECRET = 'b842244b87187fa7f98827619d5b3d4c'
 WXAPP_APPID = 'wxe986c48a87b379cd'
+
 @csrf_exempt
 def weixin_login(request):
     code = request.POST.get('code')
-    if not code:
-    	result = {'status':-1, 'msg': u'code参数不正确'}
-    	return HttpResponse(json.dumps(result), content_type='application/json')
-    if code:
-		api = WXAPPAPI(appid=APP_ID, app_secret=APP_SECRET)
-		session_info = api.exchange_code_for_session_key(code=code)
-		# 获取session_info 后
-		session_key = session_info.get('session_key')
-		openid = session_info.get('openid')
-		# crypt = WXBizDataCrypt(WXAPP_APPID, session_key)
-		# encrypted_data 包括敏感数据在内的完整用户信息的加密数据
-		# iv 加密算法的初始向量
-		# 这两个参数需要js获取
-		# user_info = crypt.decrypt(encrypted_data, iv)
-		if session_key:
-			request.session[code+'session_key'] = session_key
-			request.session[code+'openid'] = openid
-			request.session.set_expiry(30*24*60*60)
-			result = {'status':0, '_3rd_session': code, 'msg': u'登录成功'}
-			return HttpResponse(json.dumps(result), content_type='application/json')
-		else:
-			result = {'status':-1, 'msg': u'登录失败'}
-			return HttpResponse(json.dumps(result), content_type='application/json')
+    return HttpResponse(json.dumps({'request': request.POST, 'code': code}), content_type='application/json')
+  #   if not code:
+  #   	result = {'status':-1, 'msg': u'code参数不正确'}
+  #   	return HttpResponse(json.dumps(result), content_type='application/json')
+  #   if code:
+		# api = WXAPPAPI(appid=APP_ID, app_secret=APP_SECRET)
+		# session_info = api.exchange_code_for_session_key(code=code)
+		# # 获取session_info 后
+		# session_key = session_info.get('session_key')
+		# openid = session_info.get('openid')
+		# # crypt = WXBizDataCrypt(WXAPP_APPID, session_key)
+		# # encrypted_data 包括敏感数据在内的完整用户信息的加密数据
+		# # iv 加密算法的初始向量
+		# # 这两个参数需要js获取
+		# # user_info = crypt.decrypt(encrypted_data, iv)
+		# if session_key:
+		# 	request.session[code+'session_key'] = session_key
+		# 	request.session[code+'openid'] = openid
+		# 	request.session.set_expiry(30*24*60*60)
+		# 	result = {'status':0, '_3rd_session': code, 'msg': u'登录成功'}
+		# 	return HttpResponse(json.dumps(result), content_type='application/json')
+		# else:
+		# 	result = {'status':-1, 'msg': u'登录失败'}
+		# 	return HttpResponse(json.dumps(result), content_type='application/json')
 
 @csrf_exempt
 def check_session(request):
